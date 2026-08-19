c9bcb6a324321c4d6cf8d217886b2c8ad5ae8094
OmPrakash Rathore <omr31997@gmail.com>
2026-08-20 02:08:20 +0530

    first commit-1

--- src/main.ts
+++ src/main.ts
@@ -25,7 +25,7 @@ async function bootstrap() {
   );
 
   const swaggerConfig = new DocumentBuilder()
-    .setTitle('Todo & Health API')
+    .setTitle(':: Todo & Health API ::')
     .setDescription('Swagger API documentation for Todo CRUD and Health check endpoints')
     .setVersion('1.0')
     .addBearerAuth(
